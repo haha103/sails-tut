f6bc89bb234f1ee9d9735b37abbf8f109bd332e6
Liu Wei <wliu272@163.com>
2014-01-14 16:42:51 +0800

    finished ep17

--- demo-app/assets/linker/js/user.show.js
+++ demo-app/assets/linker/js/user.show.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
 		$('input[name="name"]').removeAttr('disabled');
 		$('input[name="title"]').removeAttr('disabled');
 		$('input[name="email"]').removeAttr('disabled');
+    if ($('input[name="is_admin"]').attr('value') == "true") {
+      $('input[name="admin"]').removeAttr('disabled');
+    }
 		$('input[name="password"]').closest('div').removeClass('hidden');
 		$('input[name="confirmation"]').closest('div').removeClass('hidden');
 		$('button#update').closest('div').removeClass('hidden');
@@ -14,6 +17,9 @@ $(document).ready(function () {
 		$('input[name="name"]').attr('disabled', '');
 		$('input[name="title"]').attr('disabled', '');
 		$('input[name="email"]').attr('disabled', '');
+    if ($('input[name="is_admin"]').attr('value') == "true") {
+      $('input[name="admin"]').attr('disabled', '');
+    }
 		$('input[name="password"]').closest('div').addClass('hidden');
 		$('input[name="confirmation"]').closest('div').addClass('hidden');
 		$('button#update').closest('div').addClass('hidden');
